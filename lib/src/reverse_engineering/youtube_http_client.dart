d1eaae55dd6dc5d7bea0a9871370f2757a111060
sesakaaji19137 <sesaka.aji19137@student.unsika.ac.id>
2020-08-10 09:22:13 +0700

    fix bug

--- lib/src/reverse_engineering/youtube_http_client.dart
+++ lib/src/reverse_engineering/youtube_http_client.dart
@@ -18,7 +18,7 @@ class YoutubeHttpClient extends http.BaseClient {
     'x-youtube-client-version': '2.20200609.04.02',
     'x-spf-previous': 'https://www.youtube.com/',
     'x-spf-referer': 'https://www.youtube.com/',
-    'Access-Control-Allow-Origin':'*',
+    'Access-Control-Allow-Origin':'https://www.youtube.com/',
     'Access-Control-Allow-Methods':'POST,GET,DELETE,PUT,OPTIONS',
     'x-youtube-device':
         'cbr=Chrome&cbrver=81.0.4044.138&ceng=WebKit&cengver=537.36'
